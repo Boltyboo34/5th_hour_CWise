9c6fd541e546a819d246e83befed85e258658efa
Chaysen Wise <cw126172@student.wpsok.org>
2024-08-21 13:14:44 -0500

    HW2 added

---
+++ HW2.py
@@ -0,0 +1,12 @@
+#Name: Chaysen Wise
+#Class: 5th hour
+#Assignment: HW2
+
+print ("hello world")
+print ("What is your name")
+input("")
+
+x=input ("Please give me a number")
+y=input ("another one")
+sum = int(x) + int(y)
+print (sum)
