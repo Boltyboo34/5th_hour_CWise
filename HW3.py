e93557c3562a2d384c1e0459431509b78617f77d
Chaysen Wise <cw126172@student.wpsok.org>
2024-08-29 13:07:30 -0500

    HW3 added

---
+++ HW3.py
@@ -0,0 +1,14 @@
+#Name: Chaysen Wise
+#Class: 5th hour
+#Assignment: HW3
+
+print ("hello world")
+coolList = ["bingus","Kevin","kyle","me","bingus2"]
+coolList.append(input("please enter a name"))
+print (f"{coolList[3]}")
+awesomeList = [2,1,6,12]
+awesomeList.insert(1,126)
+print (awesomeList)
+awesomeList.sort()
+sum = awesomeList[0]+awesomeList[1]+awesomeList[2]
+print (sum)
