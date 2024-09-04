61b77a4392694822401905f796898e60a6114b94
Chaysen Wise <cw126172@student.wpsok.org>
2024-09-04 13:20:07 -0500

    playground added

---
+++ Playground.py
@@ -0,0 +1,9 @@
+#Name: Chaysen Wise
+#Class: 5th hour
+#Assignment: Playground
+
+print ("Hello World")
+
+coolList = ["bingus","Kevin","joe","me","maxwell"]
+coolList.append(input("please enter the name of someone cool"))
+print (f"{coolList[3]}")
