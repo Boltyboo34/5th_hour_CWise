42e0e10a7de618803ccda7c9310e1d471f33225a
Chaysen Wise <cw126172@student.wpsok.org>
2024-09-05 13:07:16 -0500

    playground added

--- Playground.py
+++ Playground.py
@@ -2,8 +2,17 @@
 #Class: 5th hour
 #Assignment: Playground
 
-print ("Hello World")
+print("enter the password:")
+(input())
 
-coolList = ["bingus","Kevin","joe","me","maxwell"]
-coolList.append(input("please enter the name of someone cool"))
-print (f"{coolList[3]}")
+print ("Hello Mr Mack")
+
+coolList = ["bingus","Kevin","joe","me","maxwell"," mama"]
+print ("please enter 3 names of cool people")
+coolList.append(input())
+coolList.append(input())
+coolList.append(input())
+print (coolList)
+print ("Congratulations, you made a contribution to the cool list B)")
+funnyJoke = coolList[2]+coolList[5]
+print(funnyJoke)
