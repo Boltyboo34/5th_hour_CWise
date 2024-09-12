be9fae1f84ec07cd27ddbd36972f653310262a3a
Chaysen Wise <cw126172@student.wpsok.org>
2024-09-12 13:17:56 -0500

    HW4 added

---
+++ HW4.py
@@ -0,0 +1,15 @@
+#Name: Chaysen Wise
+#Class: 5th hour
+#Assignment: HW4
+
+print ("Hello World!")
+
+carDict = {
+    "brand" : "Mazda",
+    "model" : "Miata",
+    "year" : [1994,1995,1996]
+}
+
+print (carDict["year"][2])
+
+carDict.update({"has pop ups" : "true"})
