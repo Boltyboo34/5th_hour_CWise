e19886eb119b3c11a5254e6e9d5dbeb3cb6812fb
Chaysen Wise <cw126172@student.wpsok.org>
2024-09-13 11:15:56 -0500

    HW4 added

--- HW4.py
+++ HW4.py
@@ -12,4 +12,30 @@ carDict = {
 
 print (carDict["year"][2])
 
-carDict.update({"has pop ups" : "true"})
+carDict.update({"has pop ups" : True})
+
+print (carDict)
+
+carDict.clear()
+
+print (carDict)
+
+ClassDict = {
+    "Student1" : {
+         "name" : "Kevin",
+        "grade" : "11",
+      "esports" : True,
+},
+"Student2" : {
+         "name" : "Zach",
+        "grade" : "12",
+      "esports" : False,
+},
+    "Student3": {
+ "name": "Chaysen",
+"grade": "11",
+"esports": False,
+}
+}
+
+print(ClassDict["Student1"]["name"],ClassDict["Student2"]["name"],ClassDict["Student3"]["name"],)
