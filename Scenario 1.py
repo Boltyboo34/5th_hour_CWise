6e3f3b786d8fd60f030c22d737082c6abf86f0fc
Chaysen Wise <cw126172@student.wpsok.org>
2024-09-17 13:18:21 -0500

    Scenario 1 added

--- Scenario 1.py
+++ Scenario 1.py
@@ -13,30 +13,74 @@
 #It is up to you to decide what properties
 #are important and the theme of the game.
 
+
 enemyDict = {
 "Slime" : {
-         "Attack" : "30",
-        "Defense" : "30",
-      "Speed" : "40",
+   "Attack" : "30",
+   "Defense" : "30",
+   "Speed" : "40",
 },
 "Kitsune" : {
-         "Attack" : "60",
-        "Defense" : "30",
-      "Speed" : "50",
+   "Attack" : "50",
+   "Defense" : "30",
+   "Speed" : "60",
 },
-    "Cursed Armor": {
- "Attack": "60",
-"Defense": "70",
-"Speed": "20",
+"Cursed Armor": {
+   "Attack": "60",
+   "Defense": "70",
+   "Speed": "20",
 },
-    "": {
- "Attack": "",
-"Defense": "",
-"Speed": "",
+"Mimic": {
+   "Attack": "50",
+   "Defense": "50",
+   "Speed": "30",
 },
-    "": {
- "Attack": "",
-"Defense": "",
-"Speed": "",
+"Skeleton": {
+   "Attack": "40",
+   "Defense": "40",
+   "Speed": "40",
 }
 }
+
+
+print(enemyDict)
+
+
+
+slimeNewAttack = input("choose a new value for slime's attack")
+kitsuneNewAttack = input("choose a new value for kitsune's attack")
+cursedArmorNewAttack = input("choose a new value for cursed armor's attack")
+mimicNewAttack = input("choose a new value for mimic's attack")
+skeletonNewAttack = input("choose a new value for skeleton's attack")
+
+enemyDict = {
+"Slime" : {
+   "Attack" : slimeNewAttack,
+   "Defense" : "30",
+   "Speed" : "40",
+},
+"Kitsune" : {
+   "Attack" : kitsuneNewAttack,
+   "Defense" : "30",
+   "Speed" : "60",
+},
+"Cursed Armor": {
+   "Attack": cursedArmorNewAttack,
+   "Defense": "70",
+   "Speed": "20",
+},
+"Mimic": {
+   "Attack": mimicNewAttack,
+   "Defense": "50",
+   "Speed": "30",
+},
+"Skeleton": {
+   "Attack": skeletonNewAttack,
+   "Defense": "40",
+   "Speed": "40",
+}
+}
+
+
+
+print(enemyDict)
