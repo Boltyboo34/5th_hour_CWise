e224c0dc96842ee692065678ae5629de2f228707
Chaysen Wise <cw126172@student.wpsok.org>
2024-09-18 12:48:40 -0500

    Scenario 1 update added

--- Scenario 1.py
+++ Scenario 1.py
@@ -16,28 +16,28 @@
 
 enemyDict = {
 "Slime" : {
-   "Attack" : "30",
-   "Defense" : "30",
-   "Speed" : "40",
+   "Attack" : 30,
+   "Defense" : 30,
+   "Speed" : 40,
 },
 "Kitsune" : {
-   "Attack" : "50",
-   "Defense" : "30",
-   "Speed" : "60",
+   "Attack" : 50,
+   "Defense" : 30,
+   "Speed" : 60,
 },
 "Cursed Armor": {
-   "Attack": "60",
-   "Defense": "70",
-   "Speed": "20",
+   "Attack": 60,
+   "Defense": 70,
+   "Speed": 20,
 },
 "Mimic": {
-   "Attack": "50",
-   "Defense": "50",
-   "Speed": "30",
+   "Attack": 50,
+   "Defense": 50,
+   "Speed": 30,
 },
 "Skeleton": {
-   "Attack": "40",
-   "Defense": "40",
+   "Attack": 40,
+   "Defense": 40,
    "Speed": "40",
 }
 }
@@ -47,40 +47,47 @@ print(enemyDict)
 
 
 
-slimeNewAttack = input("choose a new value for slime's attack")
-kitsuneNewAttack = input("choose a new value for kitsune's attack")
-cursedArmorNewAttack = input("choose a new value for cursed armor's attack")
-mimicNewAttack = input("choose a new value for mimic's attack")
-skeletonNewAttack = input("choose a new value for skeleton's attack")
+slimeNewAttack = int(input("choose a new value for slime's attack"))
+kitsuneNewAttack = int(input("choose a new value for kitsune's attack"))
+cursedArmorNewAttack = int(input("choose a new value for cursed armor's attack"))
+mimicNewAttack = int(input("choose a new value for mimic's attack"))
+skeletonNewAttack = int(input("choose a new value for skeleton's attack"))
 
 enemyDict = {
 "Slime" : {
    "Attack" : slimeNewAttack,
-   "Defense" : "30",
-   "Speed" : "40",
+   "Defense" : 30,
+   "Speed" : 40,
 },
 "Kitsune" : {
    "Attack" : kitsuneNewAttack,
-   "Defense" : "30",
-   "Speed" : "60",
+   "Defense" : 30,
+   "Speed" : 60,
 },
 "Cursed Armor": {
    "Attack": cursedArmorNewAttack,
-   "Defense": "70",
-   "Speed": "20",
+   "Defense": 70,
+   "Speed": 20,
 },
 "Mimic": {
    "Attack": mimicNewAttack,
-   "Defense": "50",
-   "Speed": "30",
+   "Defense": 50,
+   "Speed": 30,
 },
 "Skeleton": {
    "Attack": skeletonNewAttack,
-   "Defense": "40",
-   "Speed": "40",
+   "Defense": 40,
+   "Speed": 40,
 }
 }
 
 
 
 print(enemyDict)
+
+#UPDATE: The testers have run into some bugs with your code. Some of the
+#code seems to not work correctly. For example, one of the testers changed
+#the damage for an enemy to 30 per attack, but when they attacked the player
+#character, the health went from 100 to 10030 instead of the intended 70.
+#Your team lead has asked you to fix the bug.
+#(HINT: The player's health is stored as an integer.)
