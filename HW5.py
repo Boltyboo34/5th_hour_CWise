5f237dd7716d3bdc686e67ea7ef7bde8d888ce5a
Chaysen Wise <cw126172@student.wpsok.org>
2024-09-27 13:19:11 -0500

    Scenario 2 added

---
+++ HW5.py
@@ -0,0 +1,18 @@
+#Name: Chaysen Wise
+#Class: 5th hour
+#Assignment: HW5
+
+print ("Hello World!")
+
+sillyList = [4,5,20,]
+
+if sillyList[0] < sillyList[1]:
+   if sillyList[1] > sillyList[2]:
+       print(sillyList[1], "is the biggest number")
+elif sillyList[0] > sillyList[1]:
+      elif sillyList[0] > sillyList[2]:
+         if sillyList[0] > sillyList[2
+         print(sillyList[0], "is the biggest number")
+      else: print(sillyList[2], "is the biggest number")
+
+
