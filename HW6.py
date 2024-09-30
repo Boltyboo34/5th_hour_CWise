9e5c3d4b0064d1e007d20684cee5d9914bd25b6e
Chaysen Wise <cw126172@student.wpsok.org>
2024-09-30 13:14:05 -0500

    HW6 added

---
+++ HW6.py
@@ -0,0 +1,22 @@
+#Name: Chaysen Wise
+#Class: 5th hour
+#Assignment: HW6
+import divisible
+
+print ("Hello World!")
+
+num = 12
+import divisible
+if num is divisible :
+   if num is divisible :
+       print (num divided 2)
+       print (num divided 3)
+   else:
+       print (num is not divisible 3)
+       print (num being divided 2)
+   else:
+   if num is divisible by 3:
+       print (num is not divisible 2)
+       print (num divided 3)
+   else:
+       print (that neither is divisible 2 or 3)
