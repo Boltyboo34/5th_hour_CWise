33d89b985fdb2bd4f200c176a96ca09ba93240ed
Chaysen Wise <cw126172@student.wpsok.org>
2024-09-30 12:49:00 -0500

    HW5 added

--- HW5.py
+++ HW5.py
@@ -4,15 +4,11 @@
 
 print ("Hello World!")
 
-sillyList = [4,5,20,]
+sillyList = [1,2,12,]
 
-if sillyList[0] < sillyList[1]:
-   if sillyList[1] > sillyList[2]:
-       print(sillyList[1], "is the biggest number")
-elif sillyList[0] > sillyList[1]:
-      elif sillyList[0] > sillyList[2]:
-         if sillyList[0] > sillyList[2
-         print(sillyList[0], "is the biggest number")
-      else: print(sillyList[2], "is the biggest number")
-
-
+if sillyList[0] > sillyList[1] and sillyList[0] > sillyList[2]:
+    print(sillyList[0], "is the biggest number")
+if sillyList[1] > sillyList[0] and sillyList[1] > sillyList[2]:
+    print(sillyList[1], "is the biggest number")
+if sillyList[2] > sillyList[1] and sillyList[2] > sillyList[0]:
+    print(sillyList[2], "is the biggest number")
