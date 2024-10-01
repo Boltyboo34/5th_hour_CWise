425451e74c69009b7146c83a8c9e7d0a25f5b004
Chaysen Wise <cw126172@student.wpsok.org>
2024-10-01 13:16:50 -0500

    HW6 added

--- HW6.py
+++ HW6.py
@@ -1,22 +1,21 @@
 #Name: Chaysen Wise
 #Class: 5th hour
 #Assignment: HW6
-import divisible
 
 print ("Hello World!")
 
 num = 12
-import divisible
-if num is divisible :
-   if num is divisible :
-       print (num divided 2)
-       print (num divided 3)
-   else:
-       print (num is not divisible 3)
-       print (num being divided 2)
-   else:
-   if num is divisible by 3:
-       print (num is not divisible 2)
-       print (num divided 3)
-   else:
-       print (that neither is divisible 2 or 3)
+
+if num % 2 == 0:
+    if num % 3 == 0:
+        print (num / 3)
+        print (num / 2)
+    else:
+        print ("num is not divisible 3")
+        print (num / 2)
+else:
+    if num % 3:
+        print ("num is not /2")
+        print (num / 3)
+    else:
+        print ("num is not divisible by 2 or 3")
