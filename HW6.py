62bb10b7e1a1847e3ac35556022711ff9a1b7057
Chaysen Wise <cw126172@student.wpsok.org>
2024-10-03 12:55:18 -0500

    HW6 added

--- HW6.py
+++ HW6.py
@@ -4,18 +4,18 @@
 
 print ("Hello World!")
 
-num = 12
+num = 34636
 
 if num % 2 == 0:
     if num % 3 == 0:
         print (num / 3)
         print (num / 2)
     else:
-        print ("num is not divisible 3")
+        print ("num is not divisible by 3")
         print (num / 2)
 else:
-    if num % 3:
-        print ("num is not /2")
+    if num % 3 == 0:
+        print ("num is not divisible by 2")
         print (num / 3)
     else:
         print ("num is not divisible by 2 or 3")
