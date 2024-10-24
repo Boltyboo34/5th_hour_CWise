d087ce1a839749164098eff1d399347e2ac33918
Chaysen Wise <cw126172@student.wpsok.org>
2024-10-24 12:52:23 -0500

    HW9 added

--- HW9.py
+++ HW9.py
@@ -7,13 +7,20 @@
 #2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
 #https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
 
-temp = 62
+#if temp > 20:
+#    print("it's hot")
+#if temp > 10:
+#            print("it's mild")
+#else:
+#            print("it's cold")
+#print("Thank you")
+
+temp = 13
 
 if temp > 20:
     print("it's hot")
-    if temp < 20:
-        if temp > 10:
+if temp > 10:
             print("it's mild")
-        else:
+else:
             print("it's cold")
 print("Thank you")
