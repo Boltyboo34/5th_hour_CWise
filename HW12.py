5592843b0c35f0f72d5794a33cbd1014498cd35e
Chaysen Wise <cw126172@student.wpsok.org>
2024-11-06 13:18:14 -0600

    HW12 added

---
+++ HW12.py
@@ -0,0 +1,27 @@
+#Name: Chaysen Wise
+#Class: 5th Hour
+#Assignment: HW11
+
+
+#1. Create a for loop with variable i that counts down from 5 to 1
+#and then prints "Hello World!" at the end.
+import time
+for i in range(5,0,-1):
+    print(i)
+else:
+    print("Hello World")
+
+#2. Create a for loop that counts up and prints only even numbers
+#between 1 and 30.
+#(HINT: Look back to HW6 on how to see if a number is divisible by another)
+
+for b in range(1,31):
+    if b % 2 == 0:
+        print(b)
+
+
+
+#3. Create a for loop that prints 5 different animals from a list.
+
+#4. Create a for loop that spells out a word you input backwards.
+#(HINT: Google "How to reverse a string in Python")
--- Playground.py
+++ Playground.py
@@ -1,10 +1,10 @@
 #Name: Chaysen Wise
 #Class: 5th hour
 #Assignment: Playground
-
-input("enter the password:")
-
-print ("Hello Mr Mack")
+b=input("enter the password: ")
+if b == "michael":
+    print ("Welcome")
+else: input("Try again ")
 
 coolList = ["bingus","Kevin","joe","me","maxwell"," mama"]
 print ("please enter 3 names of cool people")
@@ -25,10 +25,12 @@ print (sum)
 
 import time
 y = 1
-while y >= 1:
-    print("are we there yet")
-    time.sleep(0.8)
-    y += 1
+print("are we there yet")
+while y == 1:
     z = input()
-    if z == "yes":
-     break
+    if z == "no":
+        print("how much longerrrrrrrr")
+else:
+    print("are we there yet")
+if z == "yes":
+    exit("otay")
