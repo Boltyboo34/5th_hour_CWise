b81cd2f4e511ef6afe7169f96040831dce398303
Chaysen Wise <cw126172@student.wpsok.org>
2024-11-07 12:52:05 -0600

    HW12 added

--- HW12.py
+++ HW12.py
@@ -1,11 +1,11 @@
 #Name: Chaysen Wise
 #Class: 5th Hour
-#Assignment: HW11
+#Assignment: HW12
 
 
 #1. Create a for loop with variable i that counts down from 5 to 1
 #and then prints "Hello World!" at the end.
-import time
+
 for i in range(5,0,-1):
     print(i)
 else:
@@ -19,9 +19,15 @@ for b in range(1,31):
     if b % 2 == 0:
         print(b)
 
-
-
 #3. Create a for loop that prints 5 different animals from a list.
+
+animals = ["doge, such wow", "maxwell the cat", "tanuki", "bird puppy", "fox"]
+for z in animals:
+    print(z)
 
 #4. Create a for loop that spells out a word you input backwards.
 #(HINT: Google "How to reverse a string in Python")
+
+sillyword = input("please enter a word") [::-1]
+for y in sillyword:
+    print(y)
